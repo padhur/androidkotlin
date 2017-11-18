1dcbbcd004f00afd518edbf75b906fa123dea65f
Padmanaban Ramasamy <pathu119@gmail.com>
2017-11-18 11:21:05 +1100

    initial commit

---
+++ app/src/main/java/testapp/android/com/androidkotlinapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package testapp.android.com.androidkotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        //val helloworld = "Heloworld";
+    }
+}
